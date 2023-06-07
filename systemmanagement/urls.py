ef45e3fd47f8cf4a0a04dee6ba05d12c0224ba74
aprosdev <realseniordev210@gmail.com>
2023-06-07 11:16:21 +0300

    Added definitions templates

--- systemmanagement/urls.py
+++ systemmanagement/urls.py
@@ -10,5 +10,14 @@ urlpatterns = [
     path('equipment/', views.equipment, name='equipment'),
     path('connections/', views.connections, name='connections'),
     path('definitions/', views.definitions, name='definitions'),
+    path('definitions/system_users', views.definitions_system_users, name='definitions_system_users'),
+    path('definitions/equipment_types', views.definitions_equipment_types, name='definitions_equipment_types'),
+    path('definitions/equipment_properties', views.definitions_equipment_properties, name='definitions_equipment_properties'),
+    path('definitions/equipment_resources', views.definitions_equipment_resources, name='definitions_equipment_resources'),
+    path('definitions/equipment_interfaces', views.definitions_equipment_interfaces, name='definitions_equipment_interfaces'),
+    path('definitions/equipment_interface_classes', views.definitions_equipment_interface_classes, name='definitions_equipment_interface_classes'),
+    path('definitions/connection_types', views.definitions_connection_types, name='definitions_connection_types'),
+    path('definitions/property_data_types', views.definitions_property_data_types, name='definitions_property_data_types'),
+    path('definitions/possible_equipment_connection_states', views.definitions_possible_equipment_connection_states, name='definitions_possible_equipment_connection_states'),
     
 ]
--- systemmanagement/views.py
+++ systemmanagement/views.py
@@ -11,29 +11,37 @@ def system(request):
     return render(request, 'system.html', context=context)
 
 def system_purchasing_overview(request):
+    page = 'system'
     context = {
-        'title': 'System Purchasing Overview'
+        'title': 'System Purchasing Overview',
+        'page': page,
     }
     
     return render(request, 'system_purchasing_overview.html', context=context)
 
 def system_purchasing_detail(request):
+    page = 'system'
     context = {
-        'title': 'System Purchasing detail'
+        'title': 'System Purchasing detail',
+        'page': page,
     }
     
     return render(request, 'system_purchasing_detail.html', context=context)
 
 def system_delivery(request):
+    page = 'system'
     context = {
-        'title': 'System Delivery'
+        'title': 'System Delivery',
+        'page': page,
     }
     
     return render(request, 'system_delivery.html', context=context)
 
 def system_state(request):
+    page = 'system'
     context = {
-        'title': 'System State'
+        'title': 'System State',
+        'page': page,
     }
     
     return render(request, 'system_state.html', context=context)
@@ -47,15 +55,6 @@ def equipment(request):
     
     return render(request, 'equipment.html', context=context)
 
-def definitions(request):
-    page = 'definitions'
-    context = {
-        'title': 'Definitions',
-        'page': page,
-    }
-    
-    return render(request, 'definitions.html', context=context)
-
 def connections(request):
     page = 'connections'
     context = {
@@ -64,3 +63,93 @@ def connections(request):
     }
     
     return render(request, 'connections.html', context=context)
+
+def definitions(request):
+    page = 'definitions'
+    context = {
+        'title': 'Definitions',
+        'page': page,
+    }
+    
+    return render(request, 'definitions_system_parameters.html', context=context)
+
+def definitions_system_users(request):
+    page = 'definitions'
+    context = {
+        'title': 'Definitions',
+        'page': page,
+    }
+    
+    return render(request, 'definitions_system_users.html', context=context)
+
+def definitions_equipment_types(request):
+    page = 'definitions'
+    context = {
+        'title': 'Definitions',
+        'page': page,
+    }
+    
+    return render(request, 'definitions_equipment_types.html', context=context)
+
+def definitions_equipment_properties(request):
+    page = 'definitions'
+    context = {
+        'title': 'Definitions',
+        'page': page,
+    }
+    
+    return render(request, 'definitions_equipment_properties.html', context=context)
+
+def definitions_equipment_resources(request):
+    page = 'definitions'
+    context = {
+        'title': 'Definitions',
+        'page': page,
+    }
+    
+    return render(request, 'definitions_equipment_resources.html', context=context)
+
+def definitions_equipment_interfaces(request):
+    page = 'definitions'
+    context = {
+        'title': 'Definitions',
+        'page': page,
+    }
+    
+    return render(request, 'definitions_equipment_interfaces.html', context=context)
+
+def definitions_equipment_interface_classes(request):
+    page = 'definitions'
+    context = {
+        'title': 'Definitions',
+        'page': page,
+    }
+    
+    return render(request, 'definitions_equipment_interface_classes.html', context=context)
+
+def definitions_connection_types(request):
+    page = 'definitions'
+    context = {
+        'title': 'Definitions',
+        'page': page,
+    }
+    
+    return render(request, 'definitions_connection_types.html', context=context)
+
+def definitions_property_data_types(request):
+    page = 'definitions'
+    context = {
+        'title': 'Definitions',
+        'page': page,
+    }
+    
+    return render(request, 'definitions_property_data_types.html', context=context)
+
+def definitions_possible_equipment_connection_states(request):
+    page = 'definitions'
+    context = {
+        'title': 'Definitions',
+        'page': page,
+    }
+    
+    return render(request, 'definitions_possible_equipment_connection_states.html', context=context)

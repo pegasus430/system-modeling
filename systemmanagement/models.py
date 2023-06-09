5477bfa8430f523c57579f1e4cd940894520a457
aprosdev <realseniordev210@gmail.com>
2023-06-09 15:29:51 +0300

    Added equipment type model

--- systemmanagement/models.py
+++ systemmanagement/models.py
@@ -4,7 +4,6 @@ from django_ltree_field.fields import LTreeField
 # Create your models here.
 class AllEquipment(models.Model):
     equipment_id = models.BigIntegerField(primary_key=True)
-    # equipment_path = models.CharField(max_length=255)
     equipment_path = LTreeField(unique=True)
     equipment_tree_level = models.IntegerField()
     equipment_sort_identifier = models.TextField()
@@ -20,3 +19,19 @@ class AllEquipment(models.Model):
     class Meta:
         managed = False
         db_table = 'all_equipment'
+
+class EquipmentType(models.Model):
+    equipment_type_id = models.BigIntegerField(primary_key=True)
+    equipment_type_path = LTreeField(unique=True)
+    equipment_type_label = models.CharField(max_length=255)
+    equipment_type_model = models.TextField()
+    equipment_type_modifier = models.TextField()
+    equipment_type_manufacturer = models.TextField()
+    equipment_type_description = models.TextField()
+    equipment_type_comment = models.TextField()
+    equipment_type_is_approved = models.BooleanField()
+    equipment_type_modified_at = models.DateTimeField()
+    equipment_type_origin_path = LTreeField()
+    class Meta:
+        managed = False
+        db_table = 'equipment_type'
